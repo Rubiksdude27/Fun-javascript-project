14073dc857c203ca8f413a2594d3b7fc960d9d87
Rubiksdude27 <100394387+Rubiksdude27@users.noreply.github.com>
2022-11-17 08:31:31 -0600

    Create reload.js

---
+++ reload.js
@@ -0,0 +1,7 @@
+javascript:
+while(true){
+var delayInMilliseconds = 1000; //1 second
+setTimeout(function() {
+  location.reload();//your code to be executed after 1 second
+}, delayInMilliseconds);
+}
