e21bd91a98f4a4a9b19ce414faf2a00226f0e461
Rubiksdude27 <100394387+Rubiksdude27@users.noreply.github.com>
2022-11-17 10:59:58 -0600

    Create I haven't decieded yet.js

---
+++ I haven't decieded yet.js
@@ -0,0 +1,12 @@
+var question = prompt("What is the answer")
+if(question == "bombs") {
+  alert("Correct");
+}
+else {
+  alert("Worng");
+  alert("You messed up");
+  while(true){
+  location.reload();
+  }
+  
+}
